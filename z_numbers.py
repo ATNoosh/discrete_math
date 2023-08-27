2aa0e37346c283788290e132f0eeaf966cf8ce0f
Administrator <tahmasebi83@yahoo.com>
2023-08-27 09:50:15 +0430

    discrete math again

---
+++ z_numbers.py
@@ -0,0 +1,10 @@
+class z_numbers:
+    def is_devideable(n, d):
+        if d == 0:
+            return False
+        if n % d > 0:
+            return False
+        return True
+
+
+print(z_numbers.is_devideable(100, 0))
