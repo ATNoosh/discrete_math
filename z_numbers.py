4271c8b2024589ad6cfe87466352827e2a30c4e4
Ali Tahmasebi <tahmasebi83@gmail.com>
2023-08-30 00:24:02 +0330

    gcd added

--- z_numbers.py
+++ z_numbers.py
@@ -1,9 +1,22 @@
 class z_numbers:
+
     def is_devideable(n, d):
         if d == 0:
             return False
         if n % d > 0:
             return False
         return True
+    
+    def gcd(self, a, b):
+        if b == 1:
+            return a
+        if a == 1:
+            return b
+        if a == 0:
+            return b
+        if b == 0:
+            return a
+        return self.gcd(self, b, a % b)
 
 print(z_numbers.is_devideable(100, 0))
+print(z_numbers.gcd(z_numbers,18888,124444))
