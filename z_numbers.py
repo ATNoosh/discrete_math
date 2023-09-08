b93b9b4aadb54759be70873be4d161d67d853ad3
Ali Tahmasebi <tahmasebi83@gmail.com>
2023-09-08 23:48:36 +0330

    products_to_num added and some cleaning code

--- z_numbers.py
+++ z_numbers.py
@@ -1,14 +1,14 @@
 import math
 class z_numbers:
 
-    def is_devideable(n, d):
+    def is_devideable(n: int, d: int)->bool:
         if d == 0:
             return False
         if n % d > 0:
             return False
         return True
     
-    def gcd_euclid(self, a, b):
+    def gcd_euclid(self, a: int, b: int)->int:
         if a ==1 or b == 1:
             return a * b
         if a == 0:
@@ -17,19 +17,16 @@ class z_numbers:
             return a
         return self.gcd_euclid(self, b, a % b)
     
-    def lcd(self, a, b):
+    def lcd(self, a: int, b: int)->int:
         return a * b / self.gcd_euclid(self, a, b)
     
-    def gcd_by_products(self, a, b):
+    def gcd_by_products(self, a: int, b: int)->list:
         a_products = self.prime_products(self, a)
         b_products = self.prime_products(self, b)
         result = []
-        
         a_counter = 0
         b_counter = 0
         while a_counter < len(a_products) and b_counter < len(b_products):
-            print(a_counter)
-            print(b_counter)
             if(a_products[a_counter][0] == b_products[b_counter][0]):
                 result.append([a_products[a_counter][0], min(a_products[a_counter][1], b_products[b_counter][1])])
                 a_counter += 1
@@ -44,13 +41,12 @@ class z_numbers:
                 b_counter += 1
                 continue
         
-        return result
-            
+        return result   
     
-    def is_modular(self, a, b, m):
+    def is_modular(self, a: int, b: int, m: int)->bool:
         return self.is_devideable(a - b, m)
     
-    def is_prime(self, a):
+    def is_prime(self, a: int)->bool:
         if a == 1:
             return False
         for i in range(2, int(math.sqrt(a)+1)):
@@ -58,21 +54,21 @@ class z_numbers:
                 return False
         return True
     
-    def primes_between(self, a, b):
+    def primes_between(self, a: int, b: int)->list:
         result = []
         for i in range(min(a, b), max(a, b)):
             if self.is_prime(self, i):
                 result.append(i)
         return result
     
-    def diviors(self, a):
+    def diviors(self, a: int)->list:
         result = []
         for i in range(1, a+1):
             if self.is_devideable(a, i):
                 result.append(i)
         return result
     
-    def prime_products(self, a):
+    def prime_products(self, a: int)->list:
         result = []
         p = 2
         while 1 == 1:
@@ -91,17 +87,25 @@ class z_numbers:
                 p = self.next_prime(self, p)
         return result
                      
-    
-    def next_prime(self, a):
+    def next_prime(self, a: int)->int:
         n = a + 1
         while 1 == 1:
             if self.is_prime(self, n):
                 return n
             n = n + 1
+    
+    def products_to_num(self, a: list)->int:
+        result = 1
+        for p in a:
+            result *= math.pow(p[0], p[1])
+        return result
+    
 a = z_numbers.prime_products(z_numbers,600)
 b = z_numbers.prime_products(z_numbers,180)
 print(a)
 print(b)
-print(z_numbers.gcd_by_products(z_numbers,180,600))
+c = z_numbers.gcd_by_products(z_numbers,180,600)
+print(c)
+print(z_numbers.products_to_num(z_numbers,c))
     
 
